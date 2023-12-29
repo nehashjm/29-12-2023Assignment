9fa8690b7ce6f4cfedceb88553281e854dec9ebb
Nehash <nehash.j.m027@gmail.com>
2023-12-29 15:03:53 +0530

    MVCRainbowSchoolProject

---
+++ Day16Assignment/Day16Assignment/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Day16Assignment.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Day16Assignment/Day16Assignment/Controllers/tasksController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Day16Assignment.Models;
+
+namespace Day16Assignment.Controllers
+{
+    public class tasksController : ApiController
+    {
+        private TasksDay17AssEntities db = new TasksDay17AssEntities();
+
+        // GET: api/tasks
+        public IQueryable<task> Gettasks()
+        {
+            return db.tasks;
+        }
+
+        // GET: api/tasks/5
+        [ResponseType(typeof(task))]
+        public IHttpActionResult Gettask(int id)
+        {
+            task task = db.tasks.Find(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(task);
+        }
+
+        // PUT: api/tasks/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Puttask(int id, task task)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != task.TId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(task).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!taskExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/tasks
+        [ResponseType(typeof(task))]
+        public IHttpActionResult Posttask(task task)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.tasks.Add(task);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (taskExists(task.TId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = task.TId }, task);
+        }
+
+        // DELETE: api/tasks/5
+        [ResponseType(typeof(task))]
+        public IHttpActionResult Deletetask(int id)
+        {
+            task task = db.tasks.Find(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            db.tasks.Remove(task);
+            db.SaveChanges();
+
+            return Ok(task);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool taskExists(int id)
+        {
+            return db.tasks.Count(e => e.TId == id) > 0;
+        }
+    }
+}
